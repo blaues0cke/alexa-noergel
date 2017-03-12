e212af4951e544dae4088dfe5a5030afac6717ef
Thomas Kekeisen <github@tk.ca.kekeisen.it>
2017-03-12 13:11:11 +0100

    Added dynamodb and made some other changes

--- src/index.js
+++ src/index.js
@@ -1,29 +1,35 @@
 'use strict';
 var Alexa = require('alexa-sdk');
 
-exports.handler = function (event, context, callback) {
-    var alexa = Alexa.handler(event, context);
-    alexa.registerHandlers(handlers);
-    alexa.execute();
-};
-
 var answers = [
     '__NOERGEL, du Schwanz',
     '__NOERGEL',
     'schon wieder?'
 ];
 
-function formatAnswer (answer) {
+var answerNames = [
+    '__NAME, du Schwanz, hör auf herumzunörgeln',
+    '__NOERGEL, __NAME'
+];
+
+function formatAnswer (answer, name) {
     var count = getRandomNumber(1, 5);
     var noergels = 'Nörgel '.repeat(count).trim();
     var formattedAnswer = answer.replace(/__NOERGEL/g, noergels);
 
+    if (name) {
+        formattedAnswer = answer.replace(/__NAME/g, noergels);
+    }
+
     return formattedAnswer;
 }
 
-function getRandomAnswer () {
-    var randomAnswer = answers[Math.floor(Math.random() * answers.length)];
-    var formattedAnswer = formatAnswer(randomAnswer);
+function getRandomAnswer (name) {
+    var target = (
+        name ? answerNames : answers
+    );
+    var randomAnswer = target[Math.floor(Math.random() * target.length)];
+    var formattedAnswer = formatAnswer(randomAnswer, name);
 
     return formattedAnswer;
 }
@@ -36,12 +42,22 @@ function getRandomNumber (min, max) {
 
 var handlers = {
     'LaunchRequest': function () {
-        this.emit('SayHello');
+        this.emit(':tell', 'test');
+        //this.emit('SayHello');
     },
     'NoergelIntent': function () {
         this.emit('SayHello')
     },
     'SayHello':      function () {
-        this.emit(':tell', getRandomAnswer());
+        var name = this.event.request.intent.slots.name;
+
+        this.emit(':tell', getRandomAnswer(name));
     }
+};
+
+exports.handler = function (event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.dynamoDBTableName = 'Noergel';
+    alexa.registerHandlers(handlers);
+    alexa.execute();
 };
