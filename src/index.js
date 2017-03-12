edf0a67d1fb2e27ca504325d1ce18f763b35d835
Thomas Kekeisen <github@tk.ca.kekeisen.it>
2017-03-12 14:32:34 +0100

    Finished the base script (enough for now)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 'use strict';
 var Alexa = require('alexa-sdk');
 
-var states = {
-    NOERGELMODE: '_NOERGELMODE'
-};
-
 var answers = [
     '__NOERGEL, du Schwanz',
     '__NOERGEL',
@@ -22,7 +18,7 @@ function formatAnswer (answer, name) {
     var formattedAnswer = answer.replace(/__NOERGEL/g, noergels);
 
     if (name) {
-        formattedAnswer = answer.replace(/__NAME/g, noergels);
+        formattedAnswer = answer.replace(/__NAME/g, name);
     }
 
     return formattedAnswer;
@@ -51,25 +47,20 @@ function getRandomNumber (min, max) {
         ) + min;
 }
 
-var newSessionHandlers = {
+var noergelHandlers = {
     'NewSession':          function () {
-        if (Object.keys(this.attributes).length === 0) {
-            this.attributes['lastNoergler'] = null;
-        }
-
-        this.handler.state = states.NOERGELMODE;
-
         var question = getDefaultAnswer();
-
-        if (this.attributes['lastNoergler']) {
-            question = question + ' Der letzte Nörgler hier war natürlich wieder ' + this.attributes['lastNoergler']
-        }
-
         this.emit(
             ':ask', question,
             'Ja oder nein, Mann!'
         );
 
+    },
+    'CorneliaIntent':      function () {
+        this.emit(':ask', 'Cornelia.');
+    },
+    'VeryYesIntent':       function () {
+        this.emit(':ask', 'Oh Gott, ja.');
     },
     'AMAZON.StopIntent':   function () {
         this.emit(':tell', getDefaultAnswer());
@@ -83,65 +74,12 @@ var newSessionHandlers = {
     'Unhandled':           function () {
         console.log('Unhandled');
 
-        this.emit(':ask', 'Ja oder nein Digger!', message);
+        this.emit(':ask', getDefaultAnswer());
     }
 };
 
-var noergelHandlers = Alexa.CreateStateHandler(
-    states.NOERGELMODE, {
-        'NewSession':          function () {
-            console.log('noergelHandlers: NewSession');
-
-            this.emit('NewSession');
-        },
-        'AMAZON.HelpIntent':   function () {
-            console.log('noergelHandlers: AMAZON.HelpIntent');
-
-            this.emit(':ask', 'Ich verrate nichts!', message);
-        },
-        'AMAZON.YesIntent':    function () {
-            console.log('noergelHandlers: AMAZON.YesIntent');
-
-            this.emit('NoergelIntent');
-        },
-        'AMAZON.NoIntent':     function () {
-            console.log('noergelHandlers: AMAZON.NoIntent');
-
-            this.emit(':tell', getDefaultAnswer());
-        },
-        'AMAZON.StopIntent':   function () {
-            console.log('noergelHandlers: AMAZON.StopIntent');
-
-            this.emit(':tell', getDefaultAnswer());
-        },
-        'AMAZON.CancelIntent': function () {
-            console.log('noergelHandlers: AMAZON.CancelIntent');
-
-            this.emit(':tell', getDefaultAnswer());
-        },
-        'SessionEndedRequest': function () {
-            console.log('noergelHandlers: SessionEndedRequest');
-
-            this.emit(':tell', getDefaultAnswer() + 'TODO');
-        },
-        'NoergelIntent':       function () {
-            console.log('noergelHandlers: NoergelIntent');
-            this.emit(':ask', getDefaultAnswer());
-            //this.attributes['guessNumber'] = Math.floor(Math.random() * 100);
-            //this.handler.state = states.GUESSMODE;
-            //77this.emit(':ask', 'Great! ' + 'Try saying a number to start the game.', 'Try saying a number.');
-        },
-        'Unhandled':           function () {
-            console.log('noergelHandlers: Unhandled');
-
-            this.emit(':ask', 'Ja oder nein Digger!', message);
-        }
-    }
-);
-
 exports.handler = function (event, context, callback) {
     var alexa = Alexa.handler(event, context);
-    alexa.dynamoDBTableName = 'Noergel';
-    alexa.registerHandlers(newSessionHandlers, noergelHandlers);
+    alexa.registerHandlers(noergelHandlers);
     alexa.execute();
 };
