c970bbf59a630907cb5cc4b850e04db407d04c6f
Thomas Kekeisen <github@tk.ca.kekeisen.it>
2017-03-12 11:58:22 +0100

    Added lambda update feature using gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const gulp = require('gulp');
+const lambda = require('gulp-awslambda');
 const zip = require('gulp-zip');
 
 gulp.task(
     'default', function () {
         gulp.src('src/*')
             .pipe(zip('build.zip'))
+            .pipe(lambda('noergel', { 'region': 'eu-west-1' }))
             .pipe(gulp.dest('dist'))
     }
 );
