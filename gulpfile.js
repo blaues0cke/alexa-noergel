cdb207c25d3c2b6ccbb432d68e51f8746bbfe3ee
Thomas Kekeisen <github@tk.ca.kekeisen.it>
2017-03-12 12:27:14 +0100

    Fixed the lambda deployment script to also deploy the aws sdk

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const zip = require('gulp-zip');
 
 gulp.task(
     'default', function () {
-        gulp.src('src/*')
+        gulp.src(['src/**/*'])
             .pipe(zip('build.zip'))
             .pipe(lambda('noergel', { 'region': 'eu-west-1' }))
             .pipe(gulp.dest('dist'))
