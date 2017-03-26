3253fadd0dfce2b2de760b20cdee0c292073e53a
Thomas Kekeisen <github@tk.ca.kekeisen.it>
2017-03-26 18:20:22 +0200

    Added some debug output

--- src/index.js
+++ src/index.js
@@ -9,41 +9,57 @@ var noergelHandlers = {
 
     // When the user said something like "Alexa stop" (without application name)
     'AMAZON.CancelIntent': function () {
-        this.emit('NoergelIntent');
+        console.log('AMAZON.CancelIntent');
+
+        this.emit(':tell', textBuilder.noergel());
     },
 
     // When the user said something like "Alexa stop" (without application name)
     'AMAZON.HelpIntent': function () {
+        console.log('AMAZON.HelpIntent');
+
         this.emit(':tell', textBuilder.replacePlaceholders(language.help));
     },
 
     // When the user said "no" to a question
     'AMAZON.NoIntent': function () {
-        this.emit('NoergelIntent');
+        console.log('AMAZON.NoIntent');
+
+        this.emit(':tell', textBuilder.noergel());
     },
 
     // When the user said "stop"
     'AMAZON.StopIntent': function () {
+        console.log('AMAZON.StopIntent');
+
         this.emit(':tell', textBuilder.replacePlaceholders(language.stop));
     },
 
     // When the user said "yes" to a question
     'AMAZON.YesIntent': function () {
-        this.emit('NoergelIntent');
+        console.log('AMAZON.YesIntent');
+
+        this.emit(':tell', textBuilder.noergel());
     },
 
     // Nörgel nörgel nörgel
-    'NoergelIntent': function () {
+    'NoergelIntent': function (loud) {
+        console.log('NoergelIntent');
+
         this.emit(':ask', textBuilder.noergel());
     },
 
     // User started the skill
     'LaunchRequest': function () {
+        console.log('LaunchRequest');
+
         this.emit('NoergelIntent');
     },
 
     // The user wants to compare two persons
     'PersonIntent': function () {
+        console.log('PersonIntent', this.event.request.intent.slots);
+
         var person = util.getRandomArrayItem(
         [
             this.event.request.intent.slots.FirstPerson.value,
@@ -55,6 +71,8 @@ var noergelHandlers = {
 
     // Unknown request
     'Unhandled': function () {
+        console.log('Unhandled');
+
         this.emit('AMAZON.StopIntent');
     }
 };
